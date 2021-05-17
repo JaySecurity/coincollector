ec5d8eb2f7cc6ce4b1c3d48add0d5d17de30fe85
Jason Nicholls <jasonnicholls1981@gmail.com>
2021-05-17 14:59:45 -0400

    move Coin class to models.py create detail view

--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Coin(models.Model):
+  title = models.CharField(max_length=100)
+  year = models.IntegerField()
+  face_value = models.FloatField()
+  appraised_value = models.FloatField()
+  notes = models.TextField(max_length=300)
+    
+  def get_appreciation_percentage(self):
+    return self.appraised_value / self.face_value
+  
+  def __str__(self):
+    return f'{self.year} - {self.title}'
--- coins/views.py
+++ coins/views.py
@@ -1,26 +1,6 @@
 from django.shortcuts import render
 
-
-class Coin():
-  def __init__(self, title, year, notes, face_value, appraised_value):
-    self.title = title
-    self.year = year
-    self.notes = notes
-    self.face_value = face_value
-    self.appraised_value = appraised_value
-    
-  def get_appreciation_percentage(self):
-    return self.appraised_value / self.face_value
-
-coins=[
-  Coin('Canadian Silver Dollar', '1956', 'Purchased 1980 for 1 dollar', 1.00, 2.10),
-  Coin('American Silver Eagle', '1986', 'Purchased 1986 for 1 dollar, Uncirculated', 1.00, 45.00),
-  Coin('American Wheat Penny', '1933', 'Found in old car.', .01, 3.79),
-  Coin('Mercury Dime', '1943', 'Found in loose change', .10 , 2.50),
-  Coin('Canadian Penny', '1981', 'Found in loose change', .01 , .01),
-  Coin('American V Nickle', '1912', 'cut in half', .05 , .02),
-
-]
+from .models import Coin
 
 
 def home(request):
@@ -30,4 +10,9 @@ def about(request):
   return render(request, 'about.html')
 
 def coins_index(request):
+  coins = Coin.objects.all()
   return render(request, 'coins/index.html', {'coins': coins})
+
+def coins_detail(request, coin_id):
+  coin = Coin.objects.get(id =  coin_id)
+  return render(request, 'coins/detail.html', {'coin': coin})
